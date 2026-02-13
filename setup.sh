3501db396ed4e4038a784d77e088481ec2a69376
MVBPinho <marcos.vb.pinho@live.com>
2026-02-13 00:17:54 -0300

    Att docker compose

--- setup.sh
+++ setup.sh
@@ -15,12 +15,10 @@ fi
 # URLs dos repositórios
 REPO_AGENDAMENTO="https://github.com/Hackathon-Fiap-TC5/ms-agendamento"
 REPO_COMPARECIMENTO="https://github.com/Hackathon-Fiap-TC5/ms-comparecimento"
-REPO_COLLETIONS="https://github.com/Hackathon-Fiap-TC5/collections"
 
 # Clonar se os diretórios não existirem
 [ ! -d "ms-agendamento" ] && git clone "REPO_AGENDAMENTO"
 [ ! -d "ms-comparecimento" ] && git clone "REPO_COMPARECIMENTO"
-[ ! -d "colletions" ] && git clone "$REPO_COLLETIONS"
 
 # URL do Gist
 GIST_RAW_URL="https://gist.githubusercontent.com/Ghustavo516/74203204973a7cd391dbde607278c083/raw/17222ac445bbc50ed389a0ef452b27a890fd83d0/docker-compose-tc5.yml"
